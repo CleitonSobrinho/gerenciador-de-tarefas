1946304e81b38db588c7a1550bfa0deb797115ab
Cleiton Sobrinho <cleiton1.life@gmail.com>
2022-10-09 17:49:33 -0300

    feat:  fetch tasks fro back end, clean useless files

--- src/App.js
+++ src/App.js
@@ -1,58 +1,46 @@
 
-import { useState, useEffect, useRef } from "react";
+import { useEffect, useState} from "react";
+import axios, { Axios } from "axios";
+
 import TaskItem from "./components/TaskItem";
 
 
 function App() {
-    const mounted = useRef(false);
-        useEffect(( ) => {
-          if (mounted.current === false) {
-              mounted.current = true ;
-          } else {
-            console.log("component was updated!")
-          }
-        })
-    const [tasks, setTasks] = useState([
-      {
-        id: "1",
-        description: "Estudar programação.",
-        isCompleted: false,
-      },
-      {
-        id: "2",
-        description: "Ler",
-        isCompleted: true,
-      }]);
+  const [tasks, setTasks] = useState([
+    {
+      id: "1",
+      description: "Estudar programação.",
+      isCompleted: false,
+    },
+    {
+      id: "2",
+      description: "Ler",
+      isCompleted: true,
+    }]);
 
-      const hendleClearTasks = () => {
-        setTasks([ ]);
+    const fechTasks = async ( ) => {
+      try {
+        const {data} = await axios.get("https://fsc-task-manager-backend.herokuapp.com/tasks")
+        setTasks(data)
+      } catch (error) {
+        console.log(error)
       }
-      
-      return(
-        <>
-          {tasks.map((task)=> (
-             <TaskItem task={task} key={task.id}/>
-          ))};
-          <button onClick={hendleClearTasks}>Clicar</button>
-        </>
-      )
+  }
+    
+    useEffect(( ) => {
+      fechTasks()
+    }, [])
+
+   
+  return (
+    <>
+      {tasks.map((task) => (
+        <TaskItem task={task} key={task.id} />
+      ))};
+    </>
+  )
 };
-  /*  const [message, setMessage] = useState('Hello word!')
 
-    const rendleChangeMessage = () => {
-        setMessage('Olá mundo!');
-
-    };
-return(
-  <>
-    <h1>{message}</h1>
-    <button onClick={rendleChangeMessage}>Change message</button>
-  </>
-)
- */
-
-
-  
 
 
 export default App;
--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,17 +1,11 @@
-import { useEffect} from "react";
-const TaskItem = ({task}) => {
-      useEffect(( ) => {
-        console.log("Component was mounted");
-        return (
-          console.log('i will unmount!')
-        )
-      })
-    return (
-        <>
-            <h1>{task.description}</h1>
-            <p>{task.isCompleted? "Completa" : "Incompleta"}</p>
-        </>
-    )
+
+const TaskItem = ({ task }) => {
+  return (
+    <>
+      <h1>{task.description}</h1>
+      <p>{task.isCompleted ? "Completa" : "Incompleta"}</p>
+    </>
+  )
 };
 
 
