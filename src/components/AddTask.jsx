948a101b5e6ec16ac907ab6455eb27bc4276dcd1
Cleiton Sobrinho <cleiton1.life@gmail.com>
2022-10-12 13:38:42 -0300

    feat: add react-alert setup

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,19 +1,44 @@
-import CustomInput from "./CustomInput"
-import "./AddTask.scss"
 import { useState } from "react";
+import { FaPlus } from "react-icons/fa";
+import { useAlert } from "react-alert";
+import axios from "axios";
+
+import CustomInput from "./CustomInput";
+import CustomButton from "./CustomButton";
+
+import "./AddTask.scss";
+
+
 
 const AddTask = () => {
-    const [task, setTask] = useState(" ");
+    const [task, setTask] = useState("");
+
+    const alert = useAlert();
 
     const onChange = (e) => {
         setTask(e.target.value);
     };
+
+    const handleTaskAddition = async () => {
+        try {
+            if (task.length === 0){
+                return alert.error("A tarefa precisa de uma descrição para ser adcionada." );
+            }
+        } catch (error) {
+            
+        }
+    };
+
     return (
         <div className="add-task-container">
             <CustomInput
                 label="Adicionar Tarefa..."
-                value=" '' "
+                value= ""
                 onChange={onChange} />
+
+            <CustomButton onClick={handleTaskAddition}>
+                <FaPlus size={14} color="#ffffff" />
+            </CustomButton>
         </div>
     )
 };
