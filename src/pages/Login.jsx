a6e67b6683f3868a6a0e5134566d83408ef38f8d
Cleiton Sobrinho <cleiton1.life@gmail.com>
2022-10-14 20:43:41 -0300

    feat: add login route

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,17 @@
-import { CustomButton } from "../components/CustomButton";
+import CustomButton  from "../components/CustomButton";
 
 import "./Login.scss"
 import logo from "../assets/images/logo-2.png";
 
-const Login = ( ) => {
-    return(
-            <div className="login-container">
-                <img src={logo} alt="Full Stack Club" />
-                <div className="button-container"> 
-                    <CustomButton>Entrar</CustomButton>
-                </div>
+const Login = () => {
+    return (
+        <div className="login-container">
+            <img src={logo} alt="Full Stack Club" />
+            <div className="button-container">
+                <CustomButton>Entrar</CustomButton>
             </div>
-                    
+        </div>
+
     )
 };
 
